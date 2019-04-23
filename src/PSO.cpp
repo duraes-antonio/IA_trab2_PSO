1cadc43a124fbc2052821f89a519bac23218600c
Antônio Carlos Durães da S <37306576+duraes-antonio@users.noreply.github.com>
2019-04-23 02:11:37 -0300

    REFACT: Doc. estruturas + leitura de dados + descrição funções

--- src/PSO.cpp
+++ src/PSO.cpp
@@ -14,6 +14,9 @@ using namespace std;
 
 Best gbest;
 
+/*Variáveis recebidas do usuário*/
+int n_particulas, n_iteracoes;
+
 /*Constantes*/
 double c1 = 2.05, c2 = 2.05;
 
@@ -24,12 +27,24 @@ uniform_int_distribution<int> rand_int(POS_MIN, POS_MAX);
 uniform_real_distribution<double> rand_double_vel(POS_MIN * V_FATOR, POS_MAX * V_FATOR);
 uniform_real_distribution<double> rand_double_pos(0, 1);
 
-double f(double x, double y){
+/*Lê a qtd. de partículas e a qtd. de iterações*/
+void ler_dados() {
+
+    printf("\nDigite o número de partículas:\n");
+    cin >> n_particulas;
+
+    printf("\nDigite o número de iterações:\n");
+    cin >> n_iteracoes;
+}
+
+/*Função fitness - Função a ter seu resultado minimizado*/
+double f(double x, double y) {
     double numerador = pow(sin(sqrt(x*x + y*y)), 2) - 0.5;
     double denominador = pow(1 + 0.001 * (x*x + y*y), 2);
     return 0.5 + (numerador / denominador);
 }
 
+/*Calcula e retorna a nova velocidade da partícula*/
 double calc_v(double pbest_pos, double current_v, double p_pos, double gbest_pos){
 
     double r1 = rand_double_pos(rand_engine);
@@ -46,23 +61,23 @@ double calc_v(double pbest_pos, double current_v, double p_pos, double gbest_pos
 }
 
 int main(){
-    int num_particulas;
-    cin >> num_particulas;
 
-    Particula ps[num_particulas];
+    ler_dados();
+
+    Particula ps[n_particulas];
     gbest.fitness = DBL_MAX;
 
-    for (int i = 0; i < num_particulas; i++){
+    for (int i = 0; i < n_particulas; i++) {
         ps[i].pos.x = rand_int(rand_engine);
         ps[i].pos.y = rand_int(rand_engine);
         ps[i].pbest.fitness = DBL_MAX;
-        ps[i].v.x = rand_double_vel(rand_engine);
-        ps[i].v.y = rand_double_vel(rand_engine);
+        ps[i].vel.x = rand_double_vel(rand_engine);
+        ps[i].vel.y = rand_double_vel(rand_engine);
     }
 
-    for (int k = 0; k < 100; k++){
+    for (int k = 0; k < n_iteracoes; k++) {
 
-        for (int i = 0; i < num_particulas; i++){
+        for (int i = 0; i < n_particulas; i++) {
             double fitness = f(ps[i].pos.x, ps[i].pos.y);
             
             if (fitness < ps[i].pbest.fitness) {
@@ -72,30 +87,29 @@ int main(){
                 if (fitness < gbest.fitness) {
                     gbest.pos = ps[i].pos;
                     gbest.fitness = fitness;
-                    
                 }
             }
 
-            ps[i].v.x = calc_v(ps[i].pbest.pos.x, ps[i].v.x, ps[i].pos.x, gbest.pos.x);
-            ps[i].v.y = calc_v(ps[i].pbest.pos.y, ps[i].v.y, ps[i].pos.y, gbest.pos.y); 
+            ps[i].vel.x = calc_v(ps[i].pbest.pos.x, ps[i].vel.x, ps[i].pos.x, gbest.pos.x);
+            ps[i].vel.y = calc_v(ps[i].pbest.pos.y, ps[i].vel.y, ps[i].pos.y, gbest.pos.y);
 
-            ps[i].pos.x = ps[i].pos.x + ps[i].v.x;
-            ps[i].pos.y = ps[i].pos.y + ps[i].v.y;
+            ps[i].pos.x = ps[i].pos.x + ps[i].vel.x;
+            ps[i].pos.y = ps[i].pos.y + ps[i].vel.y;
 
             if (ps[i].pos.x > POS_MAX) {
                 ps[i].pos.x = POS_MAX;
-                ps[i].v.x = 0;
+                ps[i].vel.x = 0;
             } else if (ps[i].pos.x < POS_MIN) {
                 ps[i].pos.x = POS_MIN;
-                ps[i].v.x = 0;
+                ps[i].vel.x = 0;
             }
 
             if (ps[i].pos.y > POS_MAX) {
                 ps[i].pos.y = POS_MAX;
-                ps[i].v.y = 0;
+                ps[i].vel.y = 0;
             } else if (ps[i].pos.y < POS_MIN) {
                 ps[i].pos.y = POS_MIN;
-                ps[i].v.y = 0;
+                ps[i].vel.y = 0;
             }
         }
         cout << "Gbest de (" << gbest.pos.x << "," << gbest.pos.y << ") = " << gbest.fitness << endl;
--- src/ia_estruturas.h
+++ src/ia_estruturas.h
@@ -5,19 +5,30 @@
 #ifndef IA_TRAB2_PSO_IA_ESTRUTURAS_H
 #define IA_TRAB2_PSO_IA_ESTRUTURAS_H
 
+/*Estrutura contendo o par de eixos X e Y;
+ *Representa a posição, um ponto em um plano 2D;*/
 struct Eixos {
     double x;
     double y;
 };
 
+/*Armazena a posição que trouxe o melhor resultado da aplicação de uma função;
+ *Fitness é o melhor valor, propriamente dito;*/
 struct Best {
     Eixos pos;
     double fitness;
 };
 
+
 struct Particula {
+
+    /*Posição atual da partícula no plano*/
     Eixos pos;
-    Eixos v;
+
+    /*Par contendo a velocidade no eixo X e a velocidade no eixo Y*/
+    Eixos vel;
+
+    /*Estrutura com a posição do melhor valor alcançado pela partícula até então*/
     Best pbest;
 };
 
